cc0b54043d926b13a789077e7108105fd32b305c
XaLVaUA <xalva.ua@gmail.com>
2020-03-19 20:38:44 +0200

    push task2

---
+++ Task2/Task2/Task2/State.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Task2
+{
+    public class State
+    {
+        public const int XSize = 3;
+        public const int YSize = 3;
+
+        public int[] Board { get; set; }
+
+        public int ZeroPosition { get; set; }
+
+        public State PrevState { get; set; }
+
+        public int WeightValue { get; set; }
+
+        public int Steps { get; set; }
+
+        public State()
+        {
+            Board = new int[XSize * YSize];
+            ZeroPosition = 0;
+            PrevState = null;
+        }
+
+        public State(State state) : this()
+        {
+            Array.Copy(state.Board, Board, Board.Length);
+            ZeroPosition = state.ZeroPosition;
+            Steps = state.Steps;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine();
+            sb.AppendLine("---");
+
+            {
+                var k = 0;
+                for (int i = 0; i < XSize; i++)
+                {
+                    for (int j = 0; j < YSize; j++)
+                    {
+                        sb.Append($"{Board[k++]} ");
+                    }
+
+                    sb.AppendLine();
+                }
+            }
+
+            sb.AppendLine("---");
+            sb.AppendLine();
+
+            return sb.ToString();
+        }
+
+        protected bool Equals(State other)
+        {
+            return Board.SequenceEqual(other.Board) && ZeroPosition == other.ZeroPosition;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((State) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Board, ZeroPosition);
+        }
+    }
+}
---
+++ Task2/Task2/Task2/Worker.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace Task2
+{
+    public class Worker
+    {
+        public State GoalState { get; set; } = GenerateGoalState();
+
+        public void Do()
+        {
+            var (result, _) = Search(GenerateStartState(), int.MaxValue);
+            PrintResult(result);
+        }
+
+        public void PrintResult(State state)
+        {
+            var stack = new Stack<State>();
+
+            var curState = state;
+
+            do
+            {
+                stack.Push(curState);
+                curState = curState.PrevState;
+            } 
+            while (curState != null);
+
+            var no = 0;
+            foreach (var st in stack)
+            {
+                Console.Write($"#{++no}");
+                Console.WriteLine(st);
+            }
+        }
+
+        public (State, int) Search(State state, int weightLimit)
+        {
+            if (state.Equals(GoalState)) return (state, weightLimit);
+            var list = FindSuccessors(state);
+
+            if (list.Count == 0)
+            {
+                return (null, int.MaxValue);
+            }
+
+            while (true)
+            {
+                list = list.OrderBy(x => x.WeightValue).ToList();
+
+                var best = list[0];
+
+                if (best.WeightValue > weightLimit)
+                {
+                    return (null, best.WeightValue);
+                }
+
+                var alternative = int.MaxValue;
+
+                if (list.Count > 1)
+                {
+                    alternative = list[1].WeightValue;
+                }
+                
+                var (result, bestWeight) = Search(best, Math.Min(weightLimit, alternative));
+                
+                best.WeightValue = bestWeight;
+
+                if (result != null)
+                {
+                    return (result, result.WeightValue);
+                }
+            }
+        }
+
+        public List<State> FindSuccessors(State state)
+        {
+            var list = new List<State>();
+
+            var candidateRight = state.ZeroPosition + 1;
+            CheckAddRowCandidate(state, candidateRight, list);
+
+            var candidateLeft = state.ZeroPosition - 1;
+            CheckAddRowCandidate(state, candidateLeft, list);
+
+            var candidateUp = state.ZeroPosition - State.YSize;
+            CheckAddColCandidate(state, candidateUp, list);
+
+            var candidateDown = state.ZeroPosition + State.YSize;
+            CheckAddColCandidate(state, candidateDown, list);
+
+            return list;
+        }
+
+        public void CheckAddRowCandidate(State state, int candidate, List<State> list)
+        {
+            var rowNo = state.ZeroPosition / State.XSize;
+            if (candidate >= 0 && state.Board.Length > candidate && candidate / State.XSize == rowNo)
+            {
+                var newState = new State(state);
+                var oldZero = newState.Board[state.ZeroPosition];
+                newState.Board[state.ZeroPosition] = state.Board[candidate];
+                newState.Board[candidate] = oldZero;
+                newState.ZeroPosition = candidate;
+                newState.PrevState = state;
+                ++newState.Steps;
+                newState.WeightValue = WeightValue(newState) + newState.Steps;
+                if (!(state.PrevState != null && state.PrevState.Equals(newState)))
+                {
+                    list.Add(newState);
+                }
+            }
+        }
+
+        public void CheckAddColCandidate(State state, int candidate, List<State> list)
+        {
+            if (candidate >= 0 && state.Board.Length > candidate)
+            {
+                var newState = new State(state);
+                var oldZero = newState.Board[state.ZeroPosition];
+                newState.Board[state.ZeroPosition] = state.Board[candidate];
+                newState.Board[candidate] = oldZero;
+                newState.ZeroPosition = candidate;
+                newState.PrevState = state;
+                ++newState.Steps;
+                newState.WeightValue = WeightValue(newState) + newState.Steps;
+                if (!(state.PrevState != null && state.PrevState.Equals(newState)))
+                {
+                    list.Add(newState);
+                }
+            }
+        }
+
+        public static State GenerateStartState()
+        {
+            var state = new State();
+            
+            var b = state.Board;
+            b[0] = 2;
+            b[1] = 8;
+            b[2] = 1;
+            b[3] = 3;
+            b[4] = 6;
+            b[5] = 4;
+            b[6] = 7;
+            b[7] = 0;
+            b[8] = 5;
+
+            state.ZeroPosition = 7;
+            state.Steps = 0;
+            state.WeightValue = int.MaxValue;
+
+            return state;
+        }
+
+        public static State GenerateGoalState()
+        {
+            var state = new State();
+
+            var b = state.Board;
+            b[0] = 1;
+            b[1] = 2;
+            b[2] = 3;
+            b[3] = 4;
+            b[4] = 0;
+            b[5] = 5;
+            b[6] = 6;
+            b[7] = 7;
+            b[8] = 8;
+
+            state.ZeroPosition = 4;
+
+            return state;
+        }
+
+        public int WeightValue(State state)
+        {
+            var value = 9;
+
+            for (int i = 0, size = GoalState.Board.Length; i < size; ++i)
+            {
+                if (GoalState.Board[i] == state.Board[i])
+                {
+                    --value;
+                }
+            }
+
+            return value;
+        }
+    }
+}
